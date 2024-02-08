e2a6c914b485c77d8a688446e43bb55bf3171b71
09Riri09 <rares.dascalescu@gmail.com>
2024-02-08 19:57:17 +0200

    ui done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { InputGroup, Form, Button, Alert } from "react-bootstrap";
+import "./App.css";
 
 function App() {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    const textToCopy = document.getElementById("shortened-url").innerText;
+    navigator.clipboard.writeText(textToCopy);
+    setCopied(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="title-section">
+        <h1>URL SHORTENER</h1>
+      </div>
+      <div className="input-section">
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="Paste your URL here"
+            aria-label="Long-URL"
+            aria-describedby="basic-addon2"
+          />
+          <Button variant="outline-secondary" id="button-addon2">
+            Shorten URL
+          </Button>
+        </InputGroup>
+      </div>
+      <div className="output-section">
+        <Alert variant="success" className="text-center">
+          <Alert.Heading>URL shortened successfully:</Alert.Heading>
+          <p id="shortened-url">Gugu gaga juju</p>
+          <hr />
+          <div className="d-flex justify-content-center mb-3">
+            <Button
+              variant="outline-success"
+              onClick={copyToClipboard}
+              disabled={copied}
+            >
+              {copied ? "Copied!" : "Copy to clipboard"}
+            </Button>
+          </div>
+        </Alert>
+      </div>
+    </>
   );
 }
 
