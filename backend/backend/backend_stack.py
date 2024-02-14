ac375bbeb8addf3386c1ab301d756b9705431b47
GabrielAgache <gabriel.agache@tractable.ai>
2024-02-14 17:54:40 +0200

    Add full backend

--- backend/backend/backend_stack.py
+++ backend/backend/backend_stack.py
@@ -2,6 +2,7 @@ import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk import (aws_apigateway as apigateway,
                      aws_s3 as s3,
+                     aws_dynamodb as dynamo_db,
                      aws_lambda as lambda_)
 from aws_cdk import Stack
 class BackendStack(Stack):
@@ -14,14 +15,27 @@ class BackendStack(Stack):
                                    code=lambda_.Code.from_asset("resources"),
                                    handler="server.main")
 
+        table = dynamo_db.Table(self, "UrlMappingTable",
+                                table_name="UrlMapping",
+                                partition_key=dynamo_db.Attribute(
+                                    name="short_form",
+                                    type=dynamo_db.AttributeType.STRING)
+                                )
+
+        table.grant_read_write_data(handler)
+
 
         api = apigateway.RestApi(self, "url-shortner-api",
                                  rest_api_name="url-shortner",
                                  description="This service is used for shorting urls")
 
-        url = api.root.add_resource("{id}")
+        url = api.root.add_resource("short")
+        put_url = api.root.add_resource("add-url")
+        
+        url = url.add_resource("{url}")
 
         get_url_integration = apigateway.LambdaIntegration(
             handler, request_templates={"application/json": '{ "statusCode": "200" }'})
 
-        api.root.add_method("GET", get_url_integration)  # GET /
+        url.add_method("GET", get_url_integration)  # GET /
+        put_url.add_method("POST", get_url_integration)
--- backend/resources/server.js
+++ backend/resources/server.js
@@ -1,24 +1,59 @@
-exports.main = async function(event, context) {
-  console.log("Path:", event.path)
+import { PutItemCommand, GetItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-  if (event.httpMethod == 'GET' && event.path == "/google") {
+const client = new DynamoDBClient({});
+
+export const handler = async function(event, context) {
+  console.log("Path:", event.path)
+  if (event.httpMethod == 'GET' && event.path.startsWith("/short")) {
+    // Split the string by '/'
+    let parts = event.path.split('/');
+    let shortUrl = parts[parts.length - 1];
+    const command = new GetItemCommand({
+      TableName: "UrlMapping",
+      Key: {
+        short_form: { S: shortUrl },
+      }})
+    const response = await client.send(command);
+    if (!response.hasOwnProperty('Item')) {
+      return {
+          statusCode: 404,
+          body: "Short URL not found"
+      };
+    }
     return {
           statusCode: 302,
           headers: {
-            Location: 'https://google.com'
+            Location: response.Item.long_form.S
           },
     };
-  } else if (event.httpMethod == 'GET' && event.path == "/facebook") {
+  } else if (event.httpMethod == 'POST' && event.path.startsWith("/add-url")) {
+    const body = JSON.parse(event.body)
+    if (!body.hasOwnProperty("url")) {
+      return {
+        statusCode: 400,
+        headers: {},
+        body: "Input should contain the url field"
+      };
+    }
+    const url = body["url"]
+    const shortUrl = Math.random().toString(36).substring(2,10);
+    const command = new PutItemCommand({
+      TableName: "UrlMapping",
+      Item: {
+        short_form: { S: shortUrl },
+        long_form: { S: url },
+      }
+    });
+    await client.send(command)
     return {
-          statusCode: 302,
-          headers: {
-            Location: 'https://facebook.com'
-          },
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify({"shortUrl": shortUrl})
     };
   }
   return {
       statusCode: 404,
       headers: {},
       body: "Not found"
-    };
+  };
 }
