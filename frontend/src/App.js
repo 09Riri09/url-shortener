45c03e54276c91c929af8feb836c25e02d748205
09Riri09 <rares.dascalescu@gmail.com>
2024-02-16 12:38:56 +0200

    mobile optimisation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,8 @@ import axios from "axios";
 
 function App() {
   const [copied, setCopied] = useState(false);
-  const [longUrl, setLongUrl] = useState (' ');
-  const [shortUrl, setShortUrl] = useState (' ');
+  const [longUrl, setLongUrl] = useState(" ");
+  const [shortUrl, setShortUrl] = useState(" ");
 
   const copyToClipboard = () => {
     const textToCopy = document.getElementById("shortened-url").innerText;
@@ -16,57 +16,59 @@ function App() {
     setCopied(true);
   };
 
-  function handleInputChange(e){
+  function handleInputChange(e) {
     return setLongUrl(e.target.value);
-     }
+  }
 
-    /*const shortenUrl = () =>{
-      fetch(`https://tinyurl.com/api-create.php?url=${longUrl}`)
-      .then (response => response.text())
-      .then(data => {
-        setShortUrl(data);
-        setCopied (false);
+  const shortenUrl = () => {
+    axios
+      .post(
+        "https://wj6p6ektw9.execute-api.eu-west-1.amazonaws.com/prod/add-url/",
+        {
+          url: longUrl,
+        }
+      )
+      .then((response) => {
+        setShortUrl(
+          `https://wj6p6ektw9.execute-api.eu-west-1.amazonaws.com/prod/short/${response.data.shortUrl}`
+        );
+        setCopied(false);
       })
-      .catch (error => console.error('Error', error));
-
-    } */
-
-    const shortenUrl = () => {
-      axios.post ('https://wj6p6ektw9.execute-api.eu-west-1.amazonaws.com/prod/add-url/',{
-        url: longUrl
-      })
-      .then(response => { 
-        setShortUrl(`https://wj6p6ektw9.execute-api.eu-west-1.amazonaws.com/prod/short/${response.data.shortUrl}`);
-        setCopied (false);
-      })
-      .catch (error => console.error('Error', error));
-    }
+      .catch((error) => console.error("Error", error));
+  };
 
   return (
     <>
       <div className="title-section">
         <h1>URL SHORTENER (kinda)</h1>
-        <p>Created by RaresDev, using a custom API.</p>
+        <p>Created by RaresDev, using React and Bootstrap components for the frontend and AWS and Javascript for the backend.</p>
       </div>
       <div className="input-section">
-        <InputGroup className="mb-3">
-          <Form.Control
-            placeholder="Paste your URL here"
-            aria-label="Long-URL"
-            aria-describedby="basic-addon2"
-            onChange = {handleInputChange}
-            value = {longUrl}
-          />
-          <Button variant="outline-secondary" id="button-addon2" onClick = {shortenUrl}>
-            Shorten URL
-          </Button>
-        </InputGroup>
+      <InputGroup className="mb-3">
+  <Form.Control
+    placeholder="Paste your URL here"
+    aria-label="Long-URL"
+    aria-describedby="basic-addon2"
+    onChange={handleInputChange}
+    value={longUrl}
+  />
+  <div className="btn-wrapper">
+    <Button
+      variant="outline-secondary"
+      id="button-addon2"
+      onClick={shortenUrl}
+    >
+      Shorten URL
+    </Button>
+  </div>
+</InputGroup>
       </div>
 
-     
       <div className="output-section">
         <Alert variant="success" className="text-center">
-          <Alert.Heading className="alertheading">Your shortened URL will appear here:</Alert.Heading>
+          <Alert.Heading className="alertheading">
+            Your shortened URL will appear here:
+          </Alert.Heading>
           <p id="shortened-url">{shortUrl}</p>
           <hr />
           <div className="d-flex justify-content-center mb-3">
@@ -80,9 +82,7 @@ function App() {
           </div>
         </Alert>
       </div>
-    </> 
-     
-     
+    </>
   );
 }
 
